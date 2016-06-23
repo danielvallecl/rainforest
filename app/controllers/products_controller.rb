2c7334c0ecca363b4aba02206ebfcf4f33c58c75
Daniel Valle <daniel.vallecl@gmail.com>
2016-06-23 10:14:40 -0400

    Testing Commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Products.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.new(product_params)
+  end
+
+  if @product.save
+    redirect_to products url
+  else
+    render :new
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to product_url(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
+end
